81965d5b62e47dd647a1b4bbadd7b818879c2a70
중휘 조 <whwndgnl@hanmail.net>
2018-11-15 20:48:17 +0900

    .

--- employee/employee/employee.cpp
+++ employee/employee/employee.cpp
@@ -10,3 +10,45 @@ using namespace std;
 - 사원들 월급의 총 합
 - 간단한 인사관리 프로그램 작성
 */
+
+class Employee
+{
+private:
+	char name[100];
+public:
+	//생성자
+	//사원 이름 출력 함수
+};
+
+class PermanentWorker : public Employee
+{
+private:
+	int salary;
+public:
+	//생성자
+	//월급을 반환하는 함수
+	//정규직 사원을 출력하는 함수
+};
+
+class EmployeeHandler
+{
+private:
+	Employee * empList[50];
+	int empNum;
+public:
+	//생성자
+	//사원등록 함수
+	//전체사원 급여정보 출력 함수
+	//이번달 총 월급 합계 함수
+	//소멸자
+};
+
+int main(void)
+{
+	EmployeeHandler handler;
+	//handler.AddEmpolyee(new PermanentWorker("홍길동", 200));
+	//handler.ShowAllSalaryInfo();
+	//handler.ShowTotalSalary
+
+	return 0;
+}
